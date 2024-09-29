fb28a6289b9273bf2e677e682c772f32599fd3d9
alexrowe99 <arowe890@gmail.com>
2024-09-30 01:02:24 +0930

    forgot to add header lol

---
+++ Server2Server.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>  // For socket functions and address structures
+#include <unistd.h>  // For close() function
+#include <fcntl.h>
+
+void connect_to_neighbour(int);
